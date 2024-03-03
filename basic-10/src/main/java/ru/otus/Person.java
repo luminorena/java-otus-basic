13588dbbf6ed970ca374c337157756d88d241a2a
luminorena <94684347+luminorena@users.noreply.github.com>
2024-03-03 16:51:55 +0300

    Added equals && hashcode

--- basic-10/src/main/java/ru/otus/Person.java
+++ basic-10/src/main/java/ru/otus/Person.java
@@ -1,5 +1,7 @@
 package ru.otus;
 
+import java.util.Objects;
+
 public final class Person {
     final String firstName;
     final String middleName;
@@ -24,5 +26,23 @@ public final class Person {
                 '}';
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
+        Person person = (Person) o;
+
+        if (!Objects.equals(firstName, person.firstName)) return false;
+        if (!Objects.equals(middleName, person.middleName)) return false;
+        return Objects.equals(lastName, person.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firstName != null ? firstName.hashCode() : 0;
+        result = 31 * result + (middleName != null ? middleName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        return result;
+    }
 }
