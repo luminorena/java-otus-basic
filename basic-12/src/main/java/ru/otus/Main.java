c3643c202a3979a81d9ca178f16b3e53cf6d07fe
luminorena <94684347+luminorena@users.noreply.github.com>
2024-03-14 01:02:18 +0300

    Fixed according to comments

--- basic-12/src/main/java/ru/otus/Main.java
+++ basic-12/src/main/java/ru/otus/Main.java
@@ -9,36 +9,43 @@ import java.util.Scanner;
 
 
 public class Main {
-
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        workWithFiles();
+        listFileContents();
+
     }
 
-    public static void workWithFiles() {
+    public static List<String> listAllFiles(){
         File file = new File("basic-12");
         List<String> list = new ArrayList<>();
-        System.out.println("Список текстовых файлов в корневой дирректории: ");
         for (File f : Objects.requireNonNull(file.listFiles())) {
             if (f.isFile() && f.getPath().endsWith(".txt")) {
                 list.add(f.getName());
             }
         }
-        System.out.println(list);
+        return list;
+    }
 
-        Scanner scanner = new Scanner(System.in);
+    public static String chooseFile(){
+        List<String> list = listAllFiles();
+        System.out.println("Список текстовых файлов в корневой дирректории:\n " + list);
         System.out.println("Введите название файла, который вы хотите изменить");
         String inputStr = scanner.nextLine();
         String chosenFile = "";
         for (int i = 0; i < list.size(); i++) {
             if (!list.contains(inputStr))
                 throw new CustomException("Файл отсутствует в списке или вы ввели некорректное значение");
-             else
+            else
                 chosenFile = inputStr;
 
         }
         System.out.println("Вы выбрали файл: " + chosenFile);
-        System.out.println("Файл " + chosenFile + " содержит следующую информацию: ");
+        return  chosenFile;
+    }
 
+    public static void listFileContents(){
+        String chosenFile = chooseFile();
+        System.out.println("Файл " + chosenFile + " содержит следующую информацию: ");
 
         try (FileInputStream fis = new FileInputStream("basic-12/" + chosenFile);
              BufferedInputStream bis = new BufferedInputStream(fis);
@@ -56,7 +63,6 @@ public class Main {
         System.out.println("\nВведите текст, который вы хотите добавить в файл:");
         String inputText = scanner.nextLine();
 
-
         try (BufferedOutputStream out = new BufferedOutputStream
                 (new FileOutputStream("basic-12/" + chosenFile, true))) {
             byte[] buffer = inputText.getBytes(StandardCharsets.UTF_8);
@@ -66,6 +72,6 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
+
 }
